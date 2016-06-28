574e5448538dfd68e2ce94f7fd12142a285068ca
mludwikowski <marpko@wp.pl>
2016-06-28 13:43:00 +0200

    poprawka

--- rezerwacja/templatetags/filters.py
+++ rezerwacja/templatetags/filters.py
@@ -1,8 +1,6 @@
 from django import template
 
 from django.core.serializers import serialize
-from django.db.models.query import QuerySet
-import simplejson as json
 from django.template import Library
 
 
@@ -21,10 +19,5 @@ def addattr(value, args):
 def to_date(value):
     return value.replace(":00+00:00","")
 
-@register.filter(name='jsonify')
-def jsonify(object):
-    if isinstance(object, QuerySet):
-        return serialize('json', object)
-    return json.dumps(object)
 
 
